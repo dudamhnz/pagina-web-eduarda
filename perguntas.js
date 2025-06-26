289b38e15adfaff23e8bd71ebca6f44b634ad6c1
Eduarda <furtado.eduarda@escola.pr.gov.br>
2025-06-26 11:40:37 +0000

    colocando perguntas e respostas

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Overwatch',
+    'O que é Overwatch?',
+    'Overwatch foi um jogo eletrônico multijogador de tiro em primeira pessoa desenvolvido e publicado pela Blizzard Entertainment.'
+);
+
+criaCartao(
+    'Overwatch',
+    'Em quais plataformas é possível jogar o jogo?',
+    ' O jogo foi lançado em 24 de maio de 2016 para Microsoft Windows, PlayStation 4 e Xbox One e em 15 de outubro de 2019 para Nintendo Switch.'
+);
+
+criaCartao(
+'Overwatch',
+'Grátis para jogar?',
+'Overwatch 1 era inicialmente um jogo pago, mas sua nova versão, o Overwatch 2, é 100% grátis para jogar sempre online com jogadores de diferentes plataformas.'
+);
+
+criaCartao(
+    'Overwatch',
+    'Mapas do jogo',
+    'O jogo traz uma proposta muito interessante, em que seus mapas são feitos baseados em países. Atualmente existem 30 mapas no jogo.'
+);
+
+criaCartao(
+    'Overwatch',
+    'Heróis',
+    'Overwatch possue atualmente 43 heróis jogáveis, sendo divididos nas categorias: tanque, dano e suporte.'
+);
+
+criaCartao(
+    'Overwatch',
+    'Modos de jogo',
+    'O jogo possui atualmente 6 modos de jogo, sendo eles: controle, escolta, flashpoint, híbrido, empurrão (push) e conflito.'
+);
+
+criaCartao(
+    'Overwatch',
+    'Curiosidade',
+    'O persongem "Hanzo" seria um ciborgue ninja com um arco e uma espada mas, com sua grande quantidade de habilidades, foi dividido em dois personagens. Assim nasceram os irmãos Shimada, o arqueiro Hanzo e o ciborgue ninja Genji.'
+);
+
+criaCartao(
+    'Overwatch',
+    'Curiosidade',
+    'O jogo se passa em uma realidade futura, com Ômnicos, seres humanos com habilidades especiais, animais com inteligência humana, etc.'
+);
+criaCartao(
+    'Overwatch',
+    'Curiosidade',
+    'O jogo possui uma boa inclusão da comunidade LGBTQIA+. Atualmente, existem 6 personagens do jogo que fazem parte da comunidade.'
+)
